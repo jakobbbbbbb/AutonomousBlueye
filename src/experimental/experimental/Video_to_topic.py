70689acf8662ea8199aa64f5a368f1888a0c1251
jakobbbbbbb <jaovst@gmail.com>
2025-02-14 13:16:08 +0000

    Added play/pause button and some other improvements

--- src/experimental/experimental/Video_to_topic.py
+++ src/experimental/experimental/Video_to_topic.py
@@ -3,6 +3,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import rclpy
 from rclpy.node import Node
+import time
 
 def nothing(x):
     pass
@@ -14,49 +15,53 @@ class VideoPublisher(Node):
         self.bridge = CvBridge()
         
         # Add routes to video files if you want to run the topics on recorded video
-        self.cap = cv2.VideoCapture('/home/nikolai/Sintef_bouy_22_03_24/autonomous_main_1.mp4') 
+        self.cap = cv2.VideoCapture('/home/ovsj/Code/Video/10m.mp4') 
 
         if not self.cap.isOpened():
             self.get_logger().error('Unable to open video file.')
             exit()
 
-        self.desired_fps = 30.0
+        self.video_fps = self.cap.get(cv2.CAP_PROP_FPS) # Finding video fps
         self.width = 1920
         self.height = 1080
+        self.paused = False # Play/Pause button
+        self.last_frame_time = time.time()
 
-        self.timer = self.create_timer(1.0 / self.desired_fps, self.timer_callback)
+        self.timer = self.create_timer(1.0 / self.video_fps, self.timer_callback)
 
         # Initialize OpenCV window and trackbar
         cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
         cv2.resizeWindow('Video', 1000, 700)
 
         self.total_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        cv2.createTrackbar('Seek', 'Video', 0, self.total_frames, nothing)
+        cv2.createTrackbar('Frames', 'Video', 0, self.total_frames, nothing)
         
         self.last_trackbar_val = 0  # Keep track of the last trackbar value to detect changes
 
     def timer_callback(self):
-        # Check if the trackbar position has changed (i.e., the user has sought to a new position)
-        trackbar_val = cv2.getTrackbarPos('Seek', 'Video')
+        key = cv2.waitKey(1) & 0xFF # adding a key input
+
+        if key == ord(' '): # Play/Pause key
+            self.paused = not self.paused
+        
+        trackbar_val = cv2.getTrackbarPos('Frames', 'Video')
         if trackbar_val != self.last_trackbar_val:
             self.cap.set(cv2.CAP_PROP_POS_FRAMES, trackbar_val)
             self.last_trackbar_val = trackbar_val
         
-        ret, frame = self.cap.read()
-        if ret:
-            # Update the trackbar position based on the current video frame
-            cv2.setTrackbarPos('Seek', 'Video', int(self.cap.get(cv2.CAP_PROP_POS_FRAMES)))
-            
-            resized_frame = cv2.resize(frame, (self.width, self.height))
-            cv2.imshow('Video', resized_frame)
-            msg = self.bridge.cv2_to_imgmsg(resized_frame, 'bgr8')
-            self.publisher_.publish(msg)
-            
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                rclpy.shutdown()
-        else:
-            self.get_logger().info('Video has ended, looping.')
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        if not self.paused:
+            ret, frame = self.cap.read()
+            if ret:
+                # Update the trackbar position based on the current video frame
+                cv2.setTrackbarPos('Frames', 'Video', int(self.cap.get(cv2.CAP_PROP_POS_FRAMES)))
+                resized_frame = cv2.resize(frame, (self.width, self.height))
+                cv2.imshow('Video', resized_frame)
+                msg = self.bridge.cv2_to_imgmsg(resized_frame, 'bgr8')
+                self.publisher_.publish(msg)
+
+            else:
+                self.get_logger().info('Video has ended, looping.')
+                self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
 def main(args=None):
     rclpy.init(args=args)
